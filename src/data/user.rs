c6a6ef3ba6bcbfa1aa5404499ce4c32800106b03
STBoyden <stboyden@stboyden.xyz>
2021-01-22 22:41:29 +0000

    Set default ID of user to u16::MAX

--- src/data/user.rs
+++ src/data/user.rs
@@ -8,11 +8,23 @@ pub struct AccountInner {
 
 pub type UserAccounts = HashMap<String, AccountInner>;
 
-#[derive(Default, Clone)]
+#[derive(Clone)]
 pub struct User {
     pub username: String,
     pub id: i32,
     pub balance: i32,
     pub perms: i32,
     pub accounts: UserAccounts,
+}
+
+impl Default for User {
+    fn default() -> Self {
+        Self {
+            id: u16::MAX as i32,
+            balance: Default::default(),
+            perms: Default::default(),
+            username: Default::default(),
+            accounts: Default::default(),
+        }
+    }
 }
